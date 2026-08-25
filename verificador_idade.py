eb1a202815c5a3b41e49d77b2533644c22fc8d38
Gilson de Oliveira <ogilson384@gmail.com>
2026-08-25 16:27:01 -0300

    Add files via upload

---
+++ verificador_idade.py
@@ -0,0 +1,10 @@
+print("===========================")
+print(" VERIFICADOR DE MAIORIDADE")
+print("===========================")
+
+idade = int(input("Digite a sua idade: "))
+
+if idade>=18:
+    print("Voce é MAIOR de idade! Já pode dirigir.")
+else:
+    print("Voce é MENOR de idade! Ainda nao pode dirigir.")
