4ccef91ced1000554808a0f1af1420cb7583a000
Gilson de Oliveira <ogilson384@gmail.com>
2026-08-25 16:45:53 -0300

    Add files via upload

---
+++ Medidor_temperatura.py
@@ -0,0 +1,11 @@
+print ("=====================")
+print("Medidor de temperatura!")
+print ("=====================")
+
+temp = float (input("Digite a temperatura aqui: "))
+if temp >=30.0:
+    print("Esta muito quente beba muita agua!")
+elif temp <= 15.0:
+    print("Esta muito frio se agasalhe muito bem!")
+else:
+    print("o clima esta agradavel")
