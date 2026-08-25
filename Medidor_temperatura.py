472523b2043aec534ca48da9430e77fad6562bc1
Gilson de Oliveira <ogilson384@gmail.com>
2026-08-25 16:51:59 -0300

    Update Medidor_temperatura.py

--- Medidor_temperatura.py
+++ Medidor_temperatura.py
@@ -4,8 +4,8 @@ print ("=====================")
 
 temp = float (input("Digite a temperatura aqui: "))
 if temp >=30.0:
-    print("Esta muito quente beba muita agua!")
+    print("Está muito quente beba muita agua!")
 elif temp <= 15.0:
-    print("Esta muito frio se agasalhe muito bem!")
+    print("Está muito frio se agasalhe muito bem!")
 else:
-    print("o clima esta agradavel")
+    print("O clima esta agradavel")
