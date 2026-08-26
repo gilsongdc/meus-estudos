652e367438936148e80e8a7cefa744134354f327
Gilson de Oliveira <ogilson384@gmail.com>
2026-08-26 15:07:41 -0300

    Add files via upload

---
+++ for_while.py
@@ -0,0 +1,9 @@
+print ("Numeros d 1 a 5 multiplicados por 2 com o bucle e for ! ")
+for numero in range (1,6):
+    print(numero * 2)
+
+print("\nNumeros d 1 a 5 multiplicados por 2  con bucle while!")
+contador = 1
+while contador <=5:
+    print(contador * 2)
+    contador += 1
