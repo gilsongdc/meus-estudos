b66da4f91a3e92bdb056a301de3fda3cdc19b58a
Gilson de Oliveira <ogilson384@gmail.com>
2026-08-26 13:10:17 -0300

    Add files via upload

---
+++ funcoes.py
@@ -0,0 +1,17 @@
+def imprime(n):
+    print(n)
+imprime("Impressão deste Texto")
+
+
+def potencia(n):
+    return n * n
+x = potencia(3)
+print(x)
+
+
+def intervalo (inic=1,fim=10):
+    for inic in range(1,fim+1):
+        print(inic)
+intervalo(1,10)
+
+
