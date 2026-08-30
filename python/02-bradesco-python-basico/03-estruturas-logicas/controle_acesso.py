1289c3c172ea7f9b9a71f629b1c041ac9ea2f3fd
Gilson de Oliveira <ogilson384@gmail.com>
2026-08-29 22:35:42 -0300

    Add files via upload

---
+++ python/02-bradesco-python-basico/03-estruturas-logicas/controle_acesso.py
@@ -0,0 +1,16 @@
+print("----------------------------------------")
+print("Controle de Acesso")
+print("----------------------------------------")
+
+nome = input("Digite seu nome: ")
+senha =int(input("Digite seu senha: "))
+
+if nome == "Gilson" and senha == 1234 :
+    print( "Acesso liberado")
+
+else:
+    print( "Acesso negado")
+
+
+
+
