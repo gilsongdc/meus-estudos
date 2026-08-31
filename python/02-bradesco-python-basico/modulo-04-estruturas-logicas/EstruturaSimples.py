85ec11a20600c6a4c4d0f4d89783a33618abe04c
Gilson de Oliveira <ogilson384@gmail.com>
2026-08-31 11:40:45 -0300

    Delete python/02-bradesco-python-basico/modulo-04-estruturas-logicas directory

--- python/02-bradesco-python-basico/modulo-04-estruturas-logicas/EstruturaSimples.py
+++
@@ -1,9 +0,0 @@
-A = input("Digite um numero: ")
-B = input("Digite outro numero: ")
-
-if (A>B):
-    aux=A
-    A=B
-    B=aux
-print("o valor da variavel A agora é: ", A)
-print(" o valor da variavel B agora é: ",B)
